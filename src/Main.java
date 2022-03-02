7c8a74183d7405ec5b4e88431d7c59ade79dd70e
Alex <61623683+alexsozontov@users.noreply.github.com>
2022-03-02 14:48:08 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
   public static void main(String[] args) {
 
-    CinemexTask task1 = new CinemexTask();
+    CinemexTask task1 = new CinimexTask();
 
     System.out.println(task1.getFactorialNum(-3));
     System.out.println(task1.checkPalindromeString("NASA"));
