1a2781dd9f7dfc9d746c7349df9c2c0a369dc47e
aleksandr_sozontov <alexderrane93@gmail.com>
2022-03-02 11:05:11 +0300

    PalindromeAndFactorialFunctions

---
+++ src/CinemexTask.java
@@ -0,0 +1,22 @@
+import java.util.stream.IntStream;
+
+public class CinemexTask {
+
+  public String checkPalindromeString(String s) {
+    String reverse = new StringBuffer(s).reverse().toString();
+    if (s.equals(reverse)) {
+      return "YES";
+    } else {
+      return "NO";
+    }
+  }
+
+  public int getFactorialNum(int n) {
+    if (n <= 1) {
+      return 1;
+    } else {
+      //Сделал стримами т.к изучаю сейчас StreamAPI;
+      return IntStream.rangeClosed(2, n).reduce((a, b) -> a * b).getAsInt();
+    }
+  }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+    CinemexTask task1 = new CinemexTask();
+
+    System.out.println(task1.getFactorialNum(-3));
+    System.out.println(task1.checkPalindromeString("NASA"));
+  }
+}
+
